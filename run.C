799a8af74cb29931c1c609fec4675d7dc8111ad8
TallyH0 <poketace@gmail.com>
2017-10-21 15:23:24 +0900

    change run.C

--- run.C
+++ run.C
@@ -2,7 +2,6 @@
 
 TH1F *h1 = new TH1F("h1","test",100,0,1);
 void run() {
-  printf("  Test exercise step1\n");
   gSystem->Load("./YTransport.so");
   YTransport *ytrans = new YTransport();
   for(int i=0; i<10000; i++) {
