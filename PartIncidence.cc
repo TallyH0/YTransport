0b6718e3247e901a23224161fdceb90c6291a6c8
TallyH0 <poketace@gmail.com>
2017-11-06 18:07:32 +0900

    PartIncidence Working

--- PartIncidence.cc
+++ PartIncidence.cc
@@ -4,3 +4,12 @@ ClassImp(PartIncidence);
 PartIncidence::PartIncidence()
 {
 }
+void PartIncidence::initialize()
+{
+    direction[2] = cos(2 * pi * gRandom->Rndm()); 
+    direction[1] = cos(2 * pi * gRandom->Rndm()); 
+	if(gRandom->Rndm() > 0.5)
+	direction[0] = cos(-(1 - sqrt(direction[2]*direction[2] - direction[1]*direction[1]))); 
+	else
+	direction[0] = cos(1 - sqrt(direction[2]*direction[2] - direction[1]*direction[1])); 
+}
--- PartIncidence.h
+++ PartIncidence.h
@@ -2,12 +2,23 @@
 #define PARTINCIDENCE_H
 
 #include "TVector.h"
+#include "TRandom.h"
+#include "TMath.h"
+
+#define pi TMath::Pi()
+#define sqrt TMath::Sqrt
+#define cos TMath::Cos
 
 class PartIncidence
 {
+public:
     PartIncidence();
 	virtual ~PartIncidence() {}
 	ClassDef(PartIncidence,0);
+    void initialize();
+    double direction[3];
+	double position[3];
+private:
 };
 
 #endif
