6da4bf6081ca36acaa77f8c3b22974de6f7d4c2b
TallyH0 <poketace@gmail.com>
2017-11-07 22:19:52 +0900

    delete other file

--- AutoDict_vector_Efield__cxx.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-AutoDict_vector_Efield__cxx.so: /home/admin/Carrier_Transport/YTransport/./test_drift.cc
-AutoDict_vector_Efield__cxx.so: /home/admin/Downloads/root/include/cintdictversion.h /home/admin/Downloads/root/include/RVersion.h
-AutoDict_vector_Efield__cxx__ROOTBUILDVERSION= 5.34/36
